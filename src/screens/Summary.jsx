555764cc7e8bddef2a727760aa1fed743f34b6e9
gsr <gshawnr@gmail.com>
2023-03-16 07:37:14 -0700

    add mui Table

--- src/screens/Summary.jsx
+++ src/screens/Summary.jsx
@@ -1,13 +1,28 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+
 import { Context as AuthContext } from "../context/authContext";
+import DataTable from "../components/Table";
+import beApi from "../api/beApi";
 
 function Summary() {
   const { state: authState } = useContext(AuthContext);
 
+  useEffect(() => {
+    try {
+      (async function () {
+        const response = await beApi.get("/summary");
+        console.log(response.data);
+      })();
+    } catch (err) {
+      console.log("Error fetching summary data", err);
+    }
+  }, []);
+
   if (!authState.isSignedIn) {
     return <h1>Please sign In</h1>;
   }
-  return <div>Summary</div>;
+  // return <div>Summary</div>;
+  return <DataTable />;
 }
 
 export default Summary;
