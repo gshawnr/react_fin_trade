5e293a615a73b0e25a85ebc9cadff18a5706c269
gsr <gshawnr@gmail.com>
2023-03-16 11:27:42 -0700

    add summary table data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -24,32 +24,6 @@ import { visuallyHidden } from "@mui/utils";
 
 import "./Table.css";
 
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
-
-const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
-];
-
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
     return -1;
@@ -64,6 +38,17 @@ function getComparator(order, orderBy) {
   return order === "desc"
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function getFormatedColumns(colArr) {
+  return colArr.map((col) => {
+    return {
+      id: col.name,
+      disablePadding: false,
+      label: col.label,
+      dataType: col.dataType || "string",
+    };
+  });
 }
 
 // Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
@@ -82,39 +67,6 @@ function stableSort(array, comparator) {
   return stabilizedThis.map((el) => el[0]);
 }
 
-const headCells = [
-  {
-    id: "name",
-    numeric: false,
-    disablePadding: true,
-    label: "Dessert (100g serving)",
-  },
-  {
-    id: "calories",
-    numeric: true,
-    disablePadding: false,
-    label: "Calories",
-  },
-  {
-    id: "fat",
-    numeric: true,
-    disablePadding: false,
-    label: "Fat (g)",
-  },
-  {
-    id: "carbs",
-    numeric: true,
-    disablePadding: false,
-    label: "Carbs (g)",
-  },
-  {
-    id: "protein",
-    numeric: true,
-    disablePadding: false,
-    label: "Protein (g)",
-  },
-];
-
 function EnhancedTableHead(props) {
   const {
     onSelectAllClick,
@@ -123,10 +75,13 @@ function EnhancedTableHead(props) {
     numSelected,
     rowCount,
     onRequestSort,
+    columns,
   } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
+
+  const formattedColumns = getFormatedColumns(columns);
 
   return (
     <TableHead>
@@ -142,10 +97,10 @@ function EnhancedTableHead(props) {
             }}
           />
         </TableCell>
-        {headCells.map((headCell) => (
+        {formattedColumns.map((headCell) => (
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
+            align="center"
             padding={headCell.disablePadding ? "none" : "normal"}
             sortDirection={orderBy === headCell.id ? order : false}
           >
@@ -169,7 +124,7 @@ function EnhancedTableHead(props) {
 }
 
 function EnhancedTableToolbar(props) {
-  const { numSelected } = props;
+  const { numSelected, handleFilter } = props;
 
   return (
     <Toolbar
@@ -201,7 +156,7 @@ function EnhancedTableToolbar(props) {
           id="tableTitle"
           component="div"
         >
-          Nutrition
+          Key Statistics
         </Typography>
       )}
       {numSelected > 0 ? (
@@ -212,7 +167,7 @@ function EnhancedTableToolbar(props) {
         </Tooltip>
       ) : (
         <Tooltip title="Filter list">
-          <IconButton>
+          <IconButton onClick={(e) => handleFilter(e)}>
             <FilterListIcon />
           </IconButton>
         </Tooltip>
@@ -221,13 +176,14 @@ function EnhancedTableToolbar(props) {
   );
 }
 
-export default function EnhancedTable() {
+export default function EnhancedTable({ rows, columns }) {
   const [order, setOrder] = useState("asc");
   const [orderBy, setOrderBy] = useState("calories");
   const [selected, setSelected] = useState([]);
   const [page, setPage] = useState(0);
   const [dense, setDense] = useState(false);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [tableColumns, setTableColumns] = useState(columns);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
@@ -237,7 +193,7 @@ export default function EnhancedTable() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
+      const newSelected = rows.map((n) => n[tableColumns[0].name]);
       setSelected(newSelected);
       return;
     }
@@ -264,6 +220,25 @@ export default function EnhancedTable() {
     setSelected(newSelected);
   };
 
+  const handleFilter = (event, name) => {
+    if (tableColumns.length < columns.length) {
+      return setTableColumns(columns);
+    }
+
+    const filterValues = [
+      "netIncome",
+      "revenue",
+      "grossProfit",
+      "ticker_year",
+      "eps",
+    ];
+    const filtered = tableColumns.filter((col) => {
+      return filterValues.includes(col.name);
+    });
+
+    setTableColumns(filtered);
+  };
+
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -286,7 +261,10 @@ export default function EnhancedTable() {
   return (
     <Box sx={{ width: "100%" }}>
       <Paper sx={{ width: "100%", mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
+        <EnhancedTableToolbar
+          numSelected={selected.length}
+          handleFilter={handleFilter}
+        />
         <TableContainer>
           <Table
             sx={{ minWidth: 750 }}
@@ -300,22 +278,23 @@ export default function EnhancedTable() {
               onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
               rowCount={rows.length}
+              columns={tableColumns}
             />
             <TableBody>
               {stableSort(rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
+                  const isItemSelected = isSelected(row.ticker_year);
                   const labelId = `enhanced-table-checkbox-${index}`;
 
                   return (
                     <TableRow
                       hover
-                      onClick={(event) => handleClick(event, row.name)}
+                      onClick={(event) => handleClick(event, row.ticker_year)}
                       role="checkbox"
                       aria-checked={isItemSelected}
                       tabIndex={-1}
-                      key={row.name}
+                      key={row.ticker_year}
                       selected={isItemSelected}
                     >
                       <TableCell padding="checkbox">
@@ -327,18 +306,15 @@ export default function EnhancedTable() {
                           }}
                         />
                       </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      {tableColumns.map((item, index) => {
+                        return (
+                          <TableCell key={index} align="center">
+                            {item.dataType === "number"
+                              ? row[item.name].toLocaleString("en-US")
+                              : row[item.name]}
+                          </TableCell>
+                        );
+                      })}
                     </TableRow>
                   );
                 })}
@@ -366,11 +342,8 @@ export default function EnhancedTable() {
       </Paper>
       <FormControlLabel
         control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
+        label="Compact"
       />
     </Box>
   );
 }
-
-// array of data
-// array of headcells
--- src/screens/Summary.jsx
+++ src/screens/Summary.jsx
@@ -6,12 +6,15 @@ import beApi from "../api/beApi";
 
 function Summary() {
   const { state: authState } = useContext(AuthContext);
+  const [rowData, setRowData] = useState([]);
 
   useEffect(() => {
     try {
       (async function () {
         const response = await beApi.get("/summary");
-        console.log(response.data);
+        if (response?.data) {
+          setRowData(response.data);
+        }
       })();
     } catch (err) {
       console.log("Error fetching summary data", err);
@@ -22,7 +25,34 @@ function Summary() {
     return <h1>Please sign In</h1>;
   }
   // return <div>Summary</div>;
-  return <DataTable />;
+  return <DataTable rows={rowData} columns={columns} />;
 }
+
+// TODO move to separate file
+const columns = [
+  { name: "ticker_year", label: "Ticker / Year" },
+  { name: "currentAssets", label: "Current Assets", dataType: "number" },
+  { name: "assets", label: "Total Assets", dataType: "number" },
+  { name: "totalDebt", label: "Total Debt", dataType: "number" },
+  { name: "liabilities", label: "Total Liabilities", dataType: "number" },
+  { name: "equity", label: "Equity", dataType: "number" },
+  { name: "revenue", label: "Revenue", dataType: "number" },
+  { name: "costOfRevenue", label: "Cost of Revenue", dataType: "number" },
+  { name: "grossProfit", label: "Gross Profit", dataType: "number" },
+  { name: "netIncome", label: "Net Income", dataType: "number" },
+  { name: "eps", label: "EPS", dataType: "number" },
+  { name: "epsDiluted", label: "EPS Diluted", dataType: "number" },
+  {
+    name: "avgSharesOutstanding",
+    label: "Avg Shares Outstanding",
+    dataType: "number",
+  },
+  {
+    name: "avgSharesOutstandingDiluted",
+    dataType: "number",
+    label: "Avg Shares Outstanding Diluted",
+    dataType: "number",
+  },
+];
 
 export default Summary;
