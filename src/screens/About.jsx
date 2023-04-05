08f6bf0d73c4671a4e83458c6ac695ec05b9ff32
gsr <gshawnr@gmail.com>
2023-04-05 08:58:20 -0700

    refactor nav, add home background and about mock content

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <h1>About</h1>;
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae
+        neque sit amet velit suscipit suscipit. Fusce posuere ex ut eros
+        bibendum tempus. Nam fermentum turpis non odio lobortis, in ultrices
+        quam vestibulum. Integer eu ultrices turpis, eget consequat dolor.
+        Aenean rhoncus lacus ut risus sodales, vel dignissim libero venenatis.
+        Fusce tristique suscipit ipsum at malesuada. Etiam vel feugiat nulla,
+        nec pulvinar magna. Sed eget tellus tellus.{" "}
+      </p>
+      <p>
+        Praesent fringilla tortor sed libero maximus, vel feugiat neque
+        faucibus. Fusce vitae dapibus nulla. Sed vestibulum felis sit amet massa
+        fringilla suscipit. Nam pulvinar, nulla vel sagittis feugiat, erat orci
+        feugiat dolor, id facilisis sapien eros id ipsum. Donec in interdum
+        turpis, id euismod nibh. Sed sed fermentum leo, vel facilisis ipsum.
+        Vestibulum sit amet quam nulla. Vestibulum laoreet enim vel odio posuere
+        tristique. Mauris venenatis, nulla ac scelerisque elementum, mauris
+        lectus convallis tortor, eget dignissim lacus ipsum a lectus. Nam
+        convallis nisl euismod, commodo enim non, facilisis enim.
+      </p>
+    </div>
+  );
 }
 
 export default About;
